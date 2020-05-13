5e0ab40f02539d391e994acdb91f44b1ac123fb0
AbnerSun <sunyujiejohn@sina.com>
2020-05-13 17:03:54 +0800

    feat: 增加收藏功能
    主要是利用了小程序的storage特性

--- pages/postDetail/postDetail.js
+++ pages/postDetail/postDetail.js
@@ -1,4 +1,4 @@
-const postsData = require('../../data/posts-data.js') 
+const postsData = require('../../data/posts-data.js')
 
 Page({
 
@@ -12,60 +12,90 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-      const postId = options.postId;
-      const postData = postsData.postContent[postId];
+  onLoad: function(options) {
+    const postId = options.postId;
+    const postData = postsData.postContent[postId];
+    this.setData({
+      postData: postData,
+      postId: postId
+    })
+
+    let postsCollected = wx.getStorageSync("postsCollected");
+    if (postsCollected) {
+      const postCollected = postsCollected[postId];
       this.setData({
-        postData: postData
+        collected: postCollected
       })
+    } else {
+      let postsCollected = {};
+      this.setData({
+        collected: false
+      })
+      postsCollected[postId] = false;
+      wx.setStorageSync('postsCollected', postsCollected)
+    }
+  },
+
+  /**
+   * 点击收藏按钮事件函数
+   */
+  onCollectionTap: function(event) {
+    let that = this;
+    let postsCollected = wx.getStorageSync("postsCollected");
+    let postCollected = postsCollected[that.data.postId];
+    this.setData({
+      collected: !postCollected 
+    })
+    postsCollected[that.data.postId] = !postCollected
+    wx.setStorageSync('postsCollected', postsCollected)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
